5e4f832730651b5d3d1f8cc74f6599dd88381fb9
blueswe <57138479+blueswe@users.noreply.github.com>
2019-10-31 09:53:54 +0100

    Update UnitConverter.java

--- src/UnitConverter.java
+++ src/UnitConverter.java
@@ -3,7 +3,7 @@ public class UnitConverter {
     public static void main(String[] args)
     {
         // 1 incehs = 2.54 centimeters
-        float inches = 5.80f;
+        float inches = 5.82f;
         float cms = inches * 2.54f;
 
         System.out.println(inches + " inches is " + String.format("%.2f", cms) + " centimeters.");
